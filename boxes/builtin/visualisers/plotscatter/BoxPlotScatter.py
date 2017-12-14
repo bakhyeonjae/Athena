7a3ec93ed766ed89664fa2b460efabc4cfb0a971
Bak Hyeonjae <bakhyeonjae@gmail.com>
2017-12-14 21:36:28 +0900

    issue resolved : plot window collide with pyside context.

--- boxes/builtin/visualisers/plotscatter/BoxPlotScatter.py
+++ boxes/builtin/visualisers/plotscatter/BoxPlotScatter.py
@@ -1,8 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from pylab import *
-import plotly.plotly as py  # tools to communicate with Plotly's server
-
 import os, sys, inspect
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -38,16 +35,5 @@ class Box(CommonModuleBox):
 
     def run(self):
         scatter = plt.figure()
+        plt.show()
 
-        colors = (i + j for j in 'o<.' for i in 'bgrcmyk')
-        labels = 'one two three four five six seven eight nine ten'.split()
-        x = linspace(0, 2*pi, 3000)
-        d = (2+random((2,3000))) * c_[sin(x), cos(x)].T
-        lg = []
-        for i, l, c  in zip(range(10), labels, colors):
-            start, stop = i * 300, (i + 1) * 300
-            handle = plot(d[0, start:stop], d[1, start:stop], c, label=l)
-            lg.append(handle)
-        show()
-
-        plot_url = py.plot_mpl(scatter, filename='mpl-docs/scatter')
