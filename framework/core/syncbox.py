7e6b046c2e3b4bde0ff010838e319ce718df41f6
Bak Hyeonjae <bakhyeonjae@gmail.com>
2018-11-18 07:14:49 +0900

    uploading and downloading is implemented.

--- framework/core/syncbox.py
+++ framework/core/syncbox.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import requests
+import json
 
 sys.path.append('../..')
 
@@ -13,10 +14,24 @@ class SyncBox(object):
     def moveBoxToRepo(self, boxPath):
         compressor = Compressor()
         compressed = compressor.compress('{}/{}'.format(self.resource.getLocalWorkSpaceDir(),boxPath),self.resource.getTempDir())
-        url = 'http://localhost:5000/workspace/put'
-        files = {'upload_file':open('{}'.format(compressed),'rb')}
+        url = 'http://localhost:5000/workspace/post'
+        data = {'path':'basic/math', 'name':'Const', 'ver':'0.0.1'}
+        files = {
+                'spec':('spec', json.dumps(data), 'application/json'),
+                'file':(os.path.basename(compressed), open('{}'.format(compressed),'rb'), 'application/octet-stream')
+                }
         response = requests.post(url, files=files)
         os.remove(compressed)
 
     def syncWorkspace(self):
-        pass
+        url = 'http://localhost:5000/workspace/list'
+        response = requests.get(url)
+        res = json.loads(response.text)
+        file_list = res['boxes']
+        self.requestFile(file_list[0])
+        
+    def requestFile(self,filename):
+        url = 'http://localhost:5000/workspace/box/{}'.format(filename)
+        res = requests.get(url)
+        with open('tmp.zip', 'wb') as f:
+            f.write(res.content)
--- framework/util/compressor.py
+++ framework/util/compressor.py
@@ -2,13 +2,15 @@ import gzip
 import shutil
 import os
 
+BOX_PACKAGE_NAME = 'box'
+
 class Compressor(object):
     def __init__(self):
         pass
 
     def compress(self, pathName, destDir):
-        shutil.make_archive('tmp', 'zip', pathName)
-        if os.path.exists('{}/tmp.zip'.format(destDir)):
-            os.remove('{}/tmp.zip'.format(destDir))
-        shutil.move('./tmp.zip', destDir)
-        return '{}/tmp.zip'.format(destDir)
+        shutil.make_archive(BOX_PACKAGE_NAME, 'zip', pathName)
+        if os.path.exists('{}/{}.zip'.format(destDir,BOX_PACKAGE_NAME)):
+            os.remove('{}/{}.zip'.format(destDir,BOX_PACKAGE_NAME))
+        shutil.move('./{}.zip'.format(BOX_PACKAGE_NAME), destDir)
+        return '{}/{}.zip'.format(destDir,BOX_PACKAGE_NAME)
